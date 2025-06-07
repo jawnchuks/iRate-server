0d1d8953e6e374363a00cfde8f3016bd19b2c474
Jawn <jawnchuks@gmail.com>
2025-06-07 02:43:27 +0100

    fix: correct Staging server URL in Swagger configuration

--- src/config/swagger.ts
+++ src/config/swagger.ts
@@ -28,7 +28,7 @@ export const swaggerConfig = new DocumentBuilder()
   )
   .addServer('http://localhost:9000', 'Local Development')
   .addServer('https://api.irate.com', 'Production')
-  .addServer('https://irate-server-production.up.railway.app/api', 'Staging')
+  .addServer('https://irate-server-production.up.railway.app/', 'Staging')
   .build();
 
 export const swaggerCustomOptions: SwaggerCustomOptions = {
